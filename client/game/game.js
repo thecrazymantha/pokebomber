b7e3cc84abb77b04835435e5c9241bc928f1685e
Chevrier Jérémy <jeremxlv@free.fr>
2017-01-04 19:31:12 +0100

    Now we can move !

--- client/game/game.js
+++ client/game/game.js
@@ -1,10 +1,10 @@
-Template.game.onRendered(function() {
+Template.game.onRendered(function(){
   Game();
 });
 
 Game = function (){
   // var data = Games.find({player.userID : Meteor.userID}).fetch()[0];
-  var data = Games.find().fetch()[0];
+  var data;
   
   // Canvas
   var layer1 = document.querySelector("#L_fixe");
@@ -18,9 +18,19 @@ Game = function (){
   var tileSize = 16;
   var objSize = tileSize * 2;
   
+      
+  var pokeTiles = [];
+  
+  
+  // Game Mechanics
+  var inputState;
+  var inputBombe = false;
+  
   // Methodes
   function init () {
     // Chargement des ressources
+    data = Games.find().fetch()[0];
+    
     var ressources = [];
     ressources.push({
       img : new Image(),
@@ -34,10 +44,76 @@ Game = function (){
         img : new Image(),
         src : "ressources/img/pokemons/" + data.player[p].pokeID + ".png"
       });
-      data.player[p].img = ressources[ressources.length -1].img;
+      pokeTiles[p] = ressources[ressources.length -1].img;
     }
     
-    Loader(ressources, [gameLoop, drawMap]);
+    Loader(ressources, [drawMap]);
+    
+    // Keys listener
+    window.addEventListener('keydown', function(event){  
+      if (event.keyCode === 37) {  
+        inputState = 37;
+        
+      } else if (event.keyCode === 38) {  
+        inputState = 38;
+        
+      } else if (event.keyCode === 39) {  
+        inputState = 39;
+        
+      } else if (event.keyCode === 40) {  
+        inputState = 40;
+        
+      }  else if (event.keyCode === 32) {  
+        inputBombe = true;
+        
+      }  
+    }, false);  
+ 
+    window.addEventListener('keyup', function(event){  
+      if (event.keyCode === inputState) {  
+        inputState = false;   
+      } else if (event.keyCode === 32) {  
+        inputBombe = false;  
+      }
+      
+    }, false);   
+    
+  }
+  
+  function controlHandler () {
+    // Move
+    if (!inputState && !inputBombe) return;
+    
+    var action = {
+      gameID : data._id,
+      userID : 0,
+      axeX : 0,
+      axeY : 0,
+      bombe: inputBombe
+    };
+    
+    if (inputState != false){
+      switch (inputState){
+        case 37: action.axeX -= 1; break; // Left
+        case 38: action.axeY -= 1; break; // Up
+        case 39: action.axeX += 1; break; // Right
+        case 40: action.axeY += 1; break; // Down
+      }
+    }    
+    
+    var act = action.axeX != 0 || action.axeY != 0 || action.bombe;
+
+    if (act){
+      console.log(action);
+      Meteor.call('actionInsert', action, function(error, result) {
+      // affiche l'erreur à l'utilisateur et s'interrompt
+        if (!result){
+          console.log("error");
+        } else {
+          console.log(result._id);
+        }
+      });
+    }
   }
   
   var drawMap = function() {
@@ -59,7 +135,6 @@ Game = function (){
     }
     
     drawWall();
-    gameLoop();
   }
   
   var drawWall = function () {
@@ -70,6 +145,8 @@ Game = function (){
         }
       }
     }
+    
+    gameLoop();
   }
   
   var drawBlock = function () {
@@ -84,13 +161,19 @@ Game = function (){
   
   var drawPoke = function () {
     for (var p in data.player){
-      ctx.drawImage(data.player[p].img, 0, data.player[p].orient * objSize, objSize, objSize, (data.player[p].x * objSize), (data.player[p].y * objSize), objSize, objSize);
+      ctx.drawImage(pokeTiles[p], 0, data.player[p].orient * objSize, objSize, objSize, (data.player[p].x * objSize), (data.player[p].y * objSize), objSize, objSize);
     }
   }
+
   
-  var gameLoop = function() {
+  var gameLoop = function() {  
+    data = Games.find().fetch()[0];
+    ctx.clearRect(0,0, layer2.width, layer2.height);
     drawBlock ();
     drawPoke ();
+    
+    // check moves
+    controlHandler();
     
     requestAnimationFrame(gameLoop);
   }
